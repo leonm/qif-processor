927783676fc1d56f3434f1bfc5ce21c373d5b8f2
Leon Messerschmidt <leon.messerschmidt@gmail.com>
2015-06-08 11:33:46 +1000

    Implemented delete processor with regular expressions (tests & refactor to follow)

--- src/github.com/leonm/qp/qp.go
+++ src/github.com/leonm/qp/qp.go
@@ -2,13 +2,42 @@ package main
 
 import "github.com/codegangsta/cli"
 import "os"
+import "strings"
+import "regexp"
 
-func processDelete(c *cli.Context)  {
+func getColumn(transaction []string, column string) string {
+  for _,t := range transaction {
+    if strings.HasPrefix(t,column) {
+      return t
+    }
+  }
+  return ""
+}
+
+func NewDeleteProcessor(c *cli.Context) processTransaction {
+
+  payeeMatch := func(transaction []string) bool {return false }
+
+  if(c.IsSet("payee")) {
+    regex,_ := regexp.Compile(c.String("payee"))
+    payeeMatch = func(transaction []string) bool {
+      return regex.MatchString(getColumn(transaction,"P"))
+    }
+  }
 
   processor := func (transaction []string) []string {
+    if (payeeMatch(transaction)) {
+      return nil
+    }
     return transaction
   }
 
+  return processor
+}
+
+
+func processDelete(c *cli.Context)  {
+  processor := NewDeleteProcessor(c)
   processQIF(os.Stdin, os.Stdout, processor)
 }
 
